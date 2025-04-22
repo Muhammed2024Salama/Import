92e29b534aa322e2b7d03c2a3e60a79815d0ac68
Muhammed2024Salama <devmuhammedsalama@gmail.com>
2025-04-22 22:26:09 +0200

    Implement Response Helper

---
+++ app/Helper/ResponseHelper.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Helper;
+
+use Illuminate\Http\JsonResponse;
+
+class ResponseHelper
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * @param $status string
+     * @param $message string
+     * @param $data array
+     * @param $statusCode integer
+     * @return JsonResponse
+     * Function : Common function to display success - Json Response
+     */
+    public static function success($status = 'success', $message = null, $data = [], $statusCode = 200): JsonResponse
+    {
+        return new JsonResponse(
+            [
+                'status' => $status,
+                'message' => $message,
+                'data' => $data,
+            ],
+            $statusCode
+        );
+    }
+
+    public static function sendResponse($status = 'success', $message, $data = [], $statusCode = 200)
+    {
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+            'data' => $data,
+        ], $statusCode);
+    }
+
+    /**
+     * @param $status string
+     * @param $message string
+     * @param $statusCode integer
+     * @return JsonResponse
+     * Function : Common function to display error - Json Response
+     */
+    public static function error($status = 'error', $message = null, $statusCode = 400): JsonResponse
+    {
+        return new JsonResponse(
+            [
+                'status' => $status,
+                'message' => $message,
+            ],
+            $statusCode
+        );
+    }
+}
