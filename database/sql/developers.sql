80e94195330f8107ab5a1e23f7c3f810b140c244
Muhammed2024Salama <devmuhammedsalama@gmail.com>
2025-04-22 23:50:18 +0200

    Integrate Developer Postman | Collections

--- database/sql/developers.sql
+++ database/sql/developers.sql
@@ -1,47 +1,7 @@
 INSERT INTO developers (developer_id, name, email, phone_number, website, logo, description, status)
-VALUES ('1', 'DAMAC', 'info@damacproperties.com', '+97145205400', 'https://www.damacproperties.com',
-        'https://prod-cdn.damacproperties.com/uploads/revamp/images/navigation/damac-logo.svg',
-        'DAMAC is a leading real estate development company in the Middle East, founded in 2002 and headquartered in Dubai, United Arab Emirates. The company is known for developing luxurious and high-quality projects, including residential complexes, hotels, resorts, and commercial units.',
-        'active');
-INSERT INTO developers (developer_id, name, email, phone_number, website, logo, description, status)
-VALUES ('2', 'Al marasem', 'www.almarasemdevelopment.com', '16090', 'https://www.almarasemdevelopment.com',
-        'https://www.almarasemdevelopment.com/', 'A visionary force that penetrated the development sector since 2017 and born of AlMarasem International Group’s business initiated in 1997. Our foresight is to emerge as a leading representative of Egypt’s real estate industry. We aim to create integrated, mixed-use communities that set new benchmarks for residential, retail, leisure, and entertainment offerings. We embark on a journey fueled by innovation, excellence, and commitment to redefine the standards of the real estate sector.
-Over the years, AlMarasem International for Development has effectively translated its vision into reality, leveraging its success as a main contractor and building on it, following the natural progression to a longstanding track record of success in construction to venture strongly into the world of the real estate development with their first project ‘Fifth Square’ in 2017. Their portfolio boasts several mega projects that have significantly contributed to the growth of the Egyptian market.
-As we navigate the dynamic landscape of real estate, we push boundaries, embrace innovation, and strive for enduring legacies in every project we undertake. AlMarasem International for Development is not just a name; it’s a promise of exceptional living spaces, innovative designs, and a commitment to playing a pivotal role in the future of the real estate industry, in Egypt and beyond.
-
-', 'active');
-INSERT INTO developers (developer_id, name, email, phone_number, website, logo, description, status)
-VALUES ('3', 'La Vista', 'info@lavista.com.eg', '16063', 'https://lavista.com.eg/', 'https://lavista.com.eg/',
-        'Established in 1991, by Architect Alaa Hady and Eng. Mohamed Hady, La Vista Developments prides itself in creating a new standard in resort and residential living through its outstanding record of accomplishments. With an in-house design office that includes an expert team of architects and landscape designers, La Vista Developments’ holistic approach of building new communities embraces all practices from vision to design, construction, finishing and operation, which has allowed La Vista to continually deliver the highest standards to its clients. We have amassed a large client-base who believe in our expertise and in the strong investments made with a property purchase from La Vista Developments. This trust continues to fuel our drive to grow even further and successfully deliver on schedule several projects in the Red Sea, North coast, Shorouk City, New Cairo, 6 October as well as in the New Capital. With more projects currently being developed.',
-        'active');
-INSERT INTO developers (developer_id, name, email, phone_number, website, logo, description, status)
-VALUES ('4', 'Samana', 'info@samanadevelopers.com', '+971 4 563 9510', 'www.samanadevelopers.com',
-        'https://www.samanadevelopers.com/assets/img/updated-logo/white.png',
-        'SAMANA Developers is one of the fastest growing private developers in the country with their focus being on delivering the promise to each and every customer of a quality product at an appropriate price point that maximizes the returns for investors.',
-        'active');
-
-INSERT INTO developers (developer_id, name, email, phone_number, website, logo, description, status)
-VALUES ('5', 'Object 1 ', 'info@object-1.com', ' +971 52 381 0148', 'https://www.object-1.com/',
-        'https://www.object-1.com/wp-content/themes/dubai-theme/images/logo.svg', '', 'active');
-INSERT INTO developers (developer_id, name, email, phone_number, website, logo, description, status)
-VALUES ('6', '77 SHADES ', 'info@77shadesrealestate.com  ', '+971565036301 ', 'https://77shadesrealestate.com/',
-        'https://d2pi0n2fm836iz.cloudfront.net/586242/102220241342486717abd88236b.png',
-        'Welcome to 77 Shades of Green, a pioneering real estate development company based in Dubai, where we are committed to redefining skyliving and urban living through modern and innovative design.',
-        'active');
-INSERT INTO developers (developer_id, name, email, phone_number, website, logo, description, status)
-VALUES ('7', 'AZIZI', 'info@azizidevelopments.com', '97143596673', 'https://www.azizidevelopments.com/',
-        'https://cdn-ilclhfp.nitrocdn.com/QxAGwGxsMzUSvDRIeNiJopOxczyFWxLJ/assets/images/optimized/rev-f8bd31a/developers-eg.com/ae/wp-content/uploads/2025/01/Azizi-Developments.png',
-        'We are Azizi, a leading Dubai-based developer and a passionate team of people driven by the goal of crafting beautiful homes for people like you. And, to date, we have successfully delivered thousands of homes to investors from over 100 nationalities. Our portfolio spans across Dubai’s prime residential and commercial destinations, showcasing our commitment to transparency, customer centricity, and construction-driven approach.  Currently, we\'re developing approximately 10,
-        000 units for delivery by 2025, with an additional 31, 000 +
-                                                               units planned for delivery between 2025 and 2028. We\'re known for the world\'s second-tallest skyscraper and have a strong track record in creating world-class properties in prime locations like MBR City and Palm Jumeirah.  Venturing into hospitality,
-        we plan to launch fifty 5-star hotels and one 7-star hotel,
-        enhancing your lifestyle with unparalleled experiences in Dubai. We aim to expand globally,
-        starting with an office in the UK and projects that reflect our unique approach to construction in London,
-        extending to major European countries, the USA, Australia, and Canada.We support Dubai\'s vision and are committed to creating exceptional homes that improve lives. With our proactive approach, we strive to exceed expectations, offer value for money, and ensure customer satisfaction.',
-        'active');
-
-INSERT INTO developers (developer_id, name, email, phone_number, website, logo, description, status)
-VALUES ('8','DANUBE', 'enquiry@danubeproperties.ae', '9718005757', 'https://danubeproperties.com',
-        'https://danubeproperties.com/wp-content/uploads/2024/02/header-logo.png',
-        'Danube Group has grown from a single store started in Deira, Dubai 30 years ago to one of the most trusted and household brand in the Middle-East. The multi-million dollar Group has been consistently increasing its global footprint and annual revenue supported by ever-growing family of over 3,500 staff. Danube Group is head-quartered in Dubai and operates in 9 countries across Middle-East & Asia. The Group owns many award winning business verticals. Danube Building Material is the No.1 Building Materials company of the region offering more than 25,000 products under one roof. Danube Home is the fastest growing furniture retail brand currently present across UAE, KSA, Oman, Bahrain, Qatar, Kuwait, India and Africa. Alucopanel is the only factory manufacturing A2 grade façade cladding panels in the UAE. Danube Properties the property development arm of the business is rated amongst top 5 developers in the UAE. Company’s major achievement include on time delivery of exceptional quality assets with record sales success.',
-        'active');
+VALUES ('1', 'Salama Tech', 'info@salama-tech.com', '+201001234567', 'https://salama-tech.com', 'logo.png',
+        'We build modern and scalable web apps.', 1),
+       ('2', 'Code Sparks', 'contact@codesparks.dev', '+971501112233', 'https://codesparks.dev',
+        'codesparks-logo.png', 'We are a passionate team of developers.', 1),
+       ('3', 'DevNova', 'support@devnova.org', '+15551234567', 'https://devnova.org', 'devnova.png',
+        'Innovative solutions for modern businesses.', 1);
